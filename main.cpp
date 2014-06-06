78f80fb390d85b9aa289412270f9e651d6e03b52
NoNameA 774 <nonamea.774@gmail.com>
2014-06-06 23:03:41 +0900

    WIP maker の型

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "playouter.hpp"
 
+#include "2048-cpp/board.hpp"
 #include "2048-cpp/koyone.hpp"
 
 int main(int, char**){
@@ -12,11 +13,12 @@ int main(int, char**){
 }
 
 
-
-
-
-
-
-
+// a, b, b, a
+// c, d, d, c
+// c, d, d, c
+// a, b, b, a
+std::function<int(Board::Grid)> maker4(int a, int b, int c, int d){
+    // static tableA = maker4_impl(
+}
 
 
